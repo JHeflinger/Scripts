db9551e017057a8e9aa0c5bbf08898748127478d
Jason Heflinger <jasonheflinger@gmail.com>
2025-04-19 16:58:22 -0400

    added a success message

--- commands/clone.py
+++ commands/clone.py
@@ -3,5 +3,6 @@ import urllib.request
 if __name__ == "__main__":
     try:
         urllib.request.urlretrieve("https://raw.githubusercontent.com/JHeflinger/Scripts/refs/heads/main/shell.py", "shell.py")
+        print("Successfully cloned shell to current working directory!")
     except:
         print("Failed to clone shell into current working directory. Are you sure you are connected to the internet?")
