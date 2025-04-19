1c518571f2285aa8644edeb1c72550120489b6e5
Jason Heflinger <jasonheflinger@gmail.com>
2025-04-19 17:09:50 -0400

    syntax error

--- commands/todo.py
+++ commands/todo.py
@@ -1,6 +1,6 @@
 import os
 
-if __name__ == "__main__:"
+if __name__ == "__main__":
     if os.path.isfile("todo.txt"):
         with open("todo.txt", "r") as file:
             content = file.read()
