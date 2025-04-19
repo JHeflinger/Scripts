995c299e9771a3061090e75a600471ad5d143380
Jason Heflinger <jasonheflinger@gmail.com>
2025-04-19 17:01:05 -0400

    added clean cache

---
+++ commands/cleanscache.py
@@ -0,0 +1,13 @@
+import os
+import shutil
+
+if __name__ == "__main__":
+    print("Cleaning script cache...")
+    for filename in os.listdir(".scache"):
+        path = os.path.join(".scache", filename)
+        if os.path.isfile(path):
+            os.remove(path)
+        elif os.path.isdir(path):
+            shutil.rmtree(path)
+    print("Finished cleaning script cache!")
+    
